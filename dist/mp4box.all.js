5edd0ec9133972249fd5d55455167415fa914525
cconcolato <cconcolato@netflix.com>
2023-04-13 00:05:09 +0000

    Deploying to gh-pages from @ gpac/mp4box.js@6e8a09896254778213c5c308916e771e8537d505 🚀

--- dist/mp4box.all.js
+++ dist/mp4box.all.js
@@ -2818,6 +2818,15 @@ BoxParser.createBoxCtor("btrt", function(stream) {
 	this.avgBitrate = stream.readUint32();
 });
 
+// file:src/parsing/ccst.js
+BoxParser.createFullBoxCtor("ccst", function(stream) {
+	var flags = stream.readUint8();
+	this.all_ref_pics_intra = ((flags & 0x80) == 0x80);
+	this.intra_pred_used = ((flags & 0x40) == 0x40);
+	this.max_ref_per_pic = ((flags & 0x3f) >> 2);
+	stream.readUint24();
+});
+
 // file:src/parsing/clap.js
 BoxParser.createBoxCtor("clap", function(stream) {
 	this.cleanApertureWidthN = stream.readUint32();
