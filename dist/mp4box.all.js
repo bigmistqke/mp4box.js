d1af83d37a2838b2d9de5fe2c03d3464eb1ac38a
cconcolato <cconcolato@netflix.com>
2023-10-26 19:56:39 +0000

    Deploying to gh-pages from @ gpac/mp4box.js@6ebcc41bb47b5a8a6b042d994c0aa0752844be92 🚀

--- dist/mp4box.all.js
+++ dist/mp4box.all.js
@@ -2907,7 +2907,7 @@ BoxParser.createBoxCtor("cmpd", function(stream) {
 	this.component_types = [];
 	this.component_type_urls = [];
 	for (i = 0; i < this.component_count; i++) {
-		var component_type = stream.readUint16();
+		var component_type = stream.readUint32();
 		this.component_types.push(component_type);
 		if (component_type >= 0x8000) {
 			this.component_type_urls.push(stream.readCString());
@@ -4875,31 +4875,35 @@ BoxParser.createFullBoxCtor("udes", function(stream) {
 BoxParser.createFullBoxCtor("uncC", function(stream) {
     var i;
     this.profile = stream.readUint32();
-    this.component_count = stream.readUint16();
-    this.component_index = [];
-    this.component_bit_depth_minus_one = [];
-    this.component_format = [];
-    this.component_align_size = [];
-    for (i = 0; i < this.component_count; i++) {
-        this.component_index.push(stream.readUint16());
-        this.component_bit_depth_minus_one.push(stream.readUint8());
-        this.component_format.push(stream.readUint8());
-        this.component_align_size.push(stream.readUint8());
+    if (this.version == 1) {
+        // Nothing - just the profile
+    } else if (this.version == 0) {
+        this.component_count = stream.readUint32();
+        this.component_index = [];
+        this.component_bit_depth_minus_one = [];
+        this.component_format = [];
+        this.component_align_size = [];
+        for (i = 0; i < this.component_count; i++) {
+            this.component_index.push(stream.readUint16());
+            this.component_bit_depth_minus_one.push(stream.readUint8());
+            this.component_format.push(stream.readUint8());
+            this.component_align_size.push(stream.readUint8());
+        }
+        this.sampling_type = stream.readUint8();
+        this.interleave_type = stream.readUint8();
+        this.block_size = stream.readUint8();
+        var flags = stream.readUint8();
+        this.component_little_endian = (flags >> 7) & 0x1;
+        this.block_pad_lsb = (flags >> 6) & 0x1;
+        this.block_little_endian = (flags >> 5) & 0x1;
+        this.block_reversed = (flags >> 4) & 0x1;
+        this.pad_unknown = (flags >> 3) & 0x1;
+        this.pixel_size = stream.readUint32();
+        this.row_align_size = stream.readUint32();
+        this.tile_align_size = stream.readUint32();
+        this.num_tile_cols_minus_one = stream.readUint32();
+        this.num_tile_rows_minus_one = stream.readUint32();
     }
-    this.sampling_type = stream.readUint8();
-    this.interleave_type = stream.readUint8();
-    this.block_size = stream.readUint8();
-    var flags = stream.readUint8();
-    this.component_little_endian = (flags >> 7) & 0x1;
-    this.block_pad_lsb = (flags >> 6) & 0x1;
-    this.block_little_endian = (flags >> 5) & 0x1;
-    this.block_reversed = (flags >> 4) & 0x1;
-    this.pad_unknown = (flags >> 3) & 0x1;
-    this.pixel_size = stream.readUint8();
-    this.row_align_size = stream.readUint32();
-    this.tile_align_size = stream.readUint32();
-    this.num_tile_cols_minus_one = stream.readUint32();
-    this.num_tile_rows_minus_one = stream.readUint32();
 });
 
 // file:src/parsing/url.js
